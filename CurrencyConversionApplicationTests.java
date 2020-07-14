b9e8d1f3905e0949cd4f48d3492278361d3a2903
ManikandanRajasekaran <68292392+ManikandanRajasekaran@users.noreply.github.com>
2020-07-14 22:16:12 +0530

    Add files via upload

---
+++ CurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crossPay.currencyConversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
